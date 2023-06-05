3e4f670a1e8d8a98838ddd4c5bd00b670447327c
Peter Bishop <peterbishop@Peters-MBP.hsd1.co.comcast.net>
2023-06-05 09:02:58 -0600

    minor change to instructions

--- createHierarchy.js
+++ createHierarchy.js
@@ -53,7 +53,7 @@ pm.sendRequest({
   } else {
     
 for (var i = 1; i <= numberOfFolders; i++) {
-  //specify the random name type
+  //specify the random name type for each Folder
   var folderName = pm.variables.replaceIn("{{$randomFirstName}}");
   pm.sendRequest({
     url: `https://api.clickup.com/api/v2/space/${spaceResponse.json().id}/folder`, method: 'POST', header: { 'Content-type': 'application/json', 'authorization': request.headers['authorization'] }, body: {
@@ -66,7 +66,7 @@ for (var i = 1; i <= numberOfFolders; i++) {
     } else {
       //make some Lists!
       for (var i = 1; i <= numberOfLists; i++) {
-        //specify the random name type
+        //specify the random name type for each List
         var listName = pm.variables.replaceIn("{{$randomLocale}}");
         pm.sendRequest({
           url: `https://api.clickup.com/api/v2/folder/${folderResponse.json().id}/list`, method: 'POST', header: { 'Content-type': 'application/json', 'authorization': request.headers['authorization'] }, body: {
@@ -80,7 +80,7 @@ for (var i = 1; i <= numberOfFolders; i++) {
             console.log(`list ${listResponse.json().name} created`)
             console.log("making tasks")
             for (var t = 1; t <= numberOfTasks; t++) {
-              //specify the random name type
+              //specify the random name type for each Task
               var taskName = pm.variables.replaceIn("{{$randomBs}}");
               pm.sendRequest({
                 url: `https://api.clickup.com/api/v2/list/${listResponse.json().id}/task`, method: 'POST', header: { 'Content-type': 'application/json', 'authorization': request.headers['authorization'] }, body: {
